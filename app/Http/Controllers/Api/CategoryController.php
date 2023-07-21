234947baebc34c30195df975c6a8022eaac82088
Milosz Konopka <Milosz1532@gmail.com>
2023-07-21 23:39:44 +0200

    Change CategoryController

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use App\Models\Category;
+
 
 class CategoryController extends Controller
 {
@@ -12,11 +15,22 @@ class CategoryController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
-        $categories = Category::all(); // Pobierz wszystkie kategorie z bazy danych
+{
+    $categories = Category::all(); 
 
-        return response()->json($categories); // Zwróć dane jako odpowiedź JSON
+
+    $categoriesData = [];
+    foreach ($categories as $category) {
+        $categoryData = $category->toArray();
+        
+        $categoryData['image_path'] = $category->image_path ? URL::to('/') . Storage::url($category->image_path) : null;
+
+
+        $categoriesData[] = $categoryData;
     }
+
+    return response()->json($categoriesData);
+}
 
     /**
      * Store a newly created resource in storage.
